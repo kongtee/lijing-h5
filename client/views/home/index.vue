b21597984af766f6e5559f5c91477932dba1f295
韩光天 <kongtee@foxmail.com>
2019-12-23 14:37:09 +0800

    ADD:添加【首页】《走进瑞金》《红色会议》《培训方案》《其他基地》

--- client/views/home/index.vue
+++ client/views/home/index.vue
@@ -9,7 +9,8 @@
         <div class="image-wrap">
             <img class="image" src="@Assets/images/index02.jpg">
         </div>
-        <div class="news-container">
+        <!-- 新闻动态 -->
+        <div class="section-container">
             <div class="news-header">
                 <header>新闻动态</header>
                 <div class="more"><a href="#">更多>></a></div>
@@ -53,6 +54,157 @@
                 </li>
             </ul>
         </div>
+        <!-- 新闻动态 -->
+        <!-- 走进瑞金 -->
+        <div class="section-container">
+            <div class="news-header">
+                <header>走进瑞金</header>
+            </div>
+            <ul class="college-list">
+                <li class="college-item">
+                    <a class="college-item-wrap" href="#">
+                        <div class="college-item-image">
+                            <img src="@Assets/images/index01.jpg">
+                        </div>
+                        <div class="college-item-content">瑞金沙洲坝</div>
+                    </a>
+                </li>
+                <li class="college-item">
+                    <a class="college-item-wrap" href="#">
+                        <div class="college-item-image">
+                            <img src="@Assets/images/index01.jpg">
+                        </div>
+                        <div class="college-item-content">瑞金"二苏大"</div>
+                    </a>
+                </li>
+                <li class="college-item">
+                    <a class="college-item-wrap" href="#">
+                        <div class="college-item-image">
+                            <img src="@Assets/images/index01.jpg">
+                        </div>
+                        <div class="college-item-content">于都</div>
+                    </a>
+                </li>
+                <li class="college-item">
+                    <a class="college-item-wrap" href="#">
+                        <div class="college-item-image">
+                            <img src="@Assets/images/index01.jpg">
+                        </div>
+                        <div class="college-item-content">大栢地战斗遗址</div>
+                    </a>
+                </li>
+                <li class="college-item">
+                    <a class="college-item-wrap" href="#">
+                        <div class="college-item-image">
+                            <img src="@Assets/images/index01.jpg">
+                        </div>
+                        <div class="college-item-content">古田会议纪念馆</div>
+                    </a>
+                </li>
+                <li class="college-item">
+                    <a class="college-item-wrap" href="#">
+                        <div class="college-item-image">
+                            <img src="@Assets/images/index01.jpg">
+                        </div>
+                        <div class="college-item-content">兴国</div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <!-- 走进瑞金 -->
+        <!-- 红色回忆 -->
+        <div class="section-container">
+            <div class="news-header">
+                <header>红色会议</header>
+            </div>
+            <ul class="mem-list">
+                <li class="mem-item">
+                    <a class="mem-item-wrap" href="#">习近平在中央党校（国家行政学院）中青年干部</a>
+                </li>
+                <li class="mem-item">
+                    <a class="mem-item-wrap" href="#">红色旧址为青少年成长烙上"税收"印记</a>
+                </li>
+                <li class="mem-item">
+                    <a class="mem-item-wrap" href="#">从井冈山到延安--毛泽东里奋斗史中的红色血脉</a>
+                </li>
+                <li class="mem-item">
+                    <a class="mem-item-wrap" href="#">红色旧址为青少年成长烙上"税收"印记商店还斐林试剂历史记录会计师来得及克里斯杰弗里斯</a>
+                </li>
+                <li class="mem-item">
+                    <a class="mem-item-wrap" href="#">从井冈山到延安--毛泽东里奋斗史中的红色血脉</a>
+                </li>
+            </ul>
+        </div>
+        <!-- 红色回忆 -->
+        <!-- 培训方案 -->
+        <div class="section-container">
+            <div class="news-header">
+                <header>培训方案</header>
+            </div>
+            <ul class="train-list">
+                <li class="train-item">
+                    <a class="train-item-btn train-item-two" href="#"></a>
+                </li>
+                <li class="train-item">
+                    <a class="train-item-btn train-item-three" href="#"></a>
+                </li>
+                <li class="train-item">
+                    <a class="train-item-btn train-item-four" href="#"></a>
+                </li>
+                <li class="train-item">
+                    <a class="train-item-btn train-item-five" href="#"></a>
+                </li>
+                <li class="train-item">
+                    <a class="train-item-btn train-item-six" href="#"></a>
+                </li>
+                <li class="train-item">
+                    <a class="train-item-btn train-item-seven" href="#"></a>
+                </li>
+                <li class="train-item">
+                    <a class="train-item-btn train-item-custom" href="#"></a>
+                </li>
+            </ul>
+        </div>
+        <!-- 培训方案 -->
+        <!-- 其他基地 -->
+        <div class="other-container">
+            <div class="news-header">
+                <header>其他基地</header>
+            </div>
+            <ul class="other-list">
+                <li class="other-item">
+                    <a class="other-item-wrap" href="#">
+                        <img src="@Assets/images/img01.jpg">
+                    </a>
+                </li>
+                <li class="other-item other-item-small">
+                    <a class="other-item-wrap" href="#">
+                        <img src="@Assets/images/img02.jpg">
+                    </a>
+                </li>
+                <li class="other-item other-item-small">
+                    <a class="other-item-wrap" href="#">
+                        <img src="@Assets/images/img03.jpg">
+                    </a>
+                </li>
+                <li class="other-item">
+                    <a class="other-item-wrap" href="#">
+                        <img src="@Assets/images/img04.jpg">
+                    </a>
+                </li>
+                <li class="other-item other-item-small">
+                    <a class="other-item-wrap" href="#">
+                        <img src="@Assets/images/img05.jpg">
+                    </a>
+                </li>
+                <li class="other-item other-item-small">
+                    <a class="other-item-wrap" href="#">
+                        <img src="@Assets/images/img06.jpg">
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <!-- 其他基地 -->
     </div>
 </template>
 
@@ -67,27 +219,15 @@
             }
         }
 
-        .news-container {
+        .section-container {
             margin: 20px;
             overflow: hidden;
-
-            .news-header {
-                display: flex;
-                justify-content: space-between;
-                align-items: flex-end;
-
-                header {
-                    font-size: 18px;
-                    font-weight: bold;
-                    color: #913823;
-                }
-            }
 
             .news-list {
                 padding-top: 5px;
 
                 .news-item {
-                    padding: 30px 0;
+                    padding: 15px 0;
                     border-top: 1px solid #eee;
 
                     .news-item-wrap {
@@ -128,6 +268,118 @@
 
                 .news-item:first-child {
                     border: none;
+                }
+            }
+
+            .college-list {
+                display: flex;
+                flex-wrap: wrap;
+                padding: 15px 0;
+
+                .college-item {
+                    width: 50%;
+                    margin: 15px 0 0;
+
+                    .college-item-wrap {
+                        display: flex;
+                        flex-direction: column;
+                        margin-right: 15px;
+
+                        .college-item-image {
+                            overflow: hidden;
+                            flex-shrink: 0;
+
+                            img {
+                                width: 100%;
+                            }
+                        }
+
+                        .college-item-content {
+                            text-align: center;
+                            font-size: 12px;
+                        }
+                    }
+
+                    .college-item-wrap:nth-child(even) {
+                        margin-right: 0;
+                    }
+                }
+            }
+
+            .mem-list {
+                padding: 15px 0;
+
+                .mem-item {
+                    line-height: 29px;
+                    font-size: 13px;
+                    text-overflow: ellipsis;
+                    overflow: hidden;
+                    white-space: nowrap;
+                }
+            }
+
+            .train-list {
+                display: flex;
+                justify-content: space-between;
+                margin-top: 15px;
+
+                .train-item-btn {
+                    display: block;
+                    width: 45px;
+                    height: 29px;
+                    background-size: contain;
+                }
+
+                .train-item-two {
+                    background-image: url("../../assets/images/jidi_fangan01.png");
+                }
+
+                .train-item-three {
+                    background-image: url("../../assets/images/jidi_fangan02.png");
+                }
+
+                .train-item-four {
+                    background-image: url("../../assets/images/jidi_fangan03.png");
+                }
+
+                .train-item-five {
+                    background-image: url("../../assets/images/jidi_fangan04.png");
+                }
+
+                .train-item-six {
+                    background-image: url("../../assets/images/jidi_fangan05.png");
+                }
+
+                .train-item-seven {
+                    background-image: url("../../assets/images/jidi_fangan06.png");
+                }
+
+                .train-item-custom {
+                    background-image: url("../../assets/images/jidi_fangan07.png");
+                }
+            }
+        }
+
+        .other-container {
+            padding-top: 20px;
+            overflow: hidden;
+
+            .news-header {
+                margin: 0 20px;
+            }
+
+            .other-list {
+                display: flex;
+                flex-wrap: wrap;
+                padding-top: 15px;
+
+                .other-item img {
+                    display: block;
+                    width: 38vw;
+                }
+
+                .other-item-small img {
+                    width: 31vw;
                 }
             }
         }
